2981ef03b07c43b493b07fded3b28058cf62d418
tony dugué <duguetony@gmail.com>
2022-05-18 01:08:09 +0200

    add Product module / controller / service / schema & database connexion with mongoose & Post endpoint

---
+++ src/product/product.controller.ts
@@ -0,0 +1,18 @@
+import {Body, Controller, Post} from '@nestjs/common';
+import {ProductService} from "./product.service";
+
+// localhost:3000/product
+@Controller('product')
+export class ProductController {
+  constructor(private productService: ProductService) {}
+
+  @Post()
+  createProduct(
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description?: string,
+  ) {
+    return this.productService.create(name, price, description)
+  }
+}
+
