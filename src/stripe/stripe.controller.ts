b8e23806aa16ec118a9a9486a2316e43d569dbfd
tony dugué <duguetony@gmail.com>
2022-06-01 01:14:16 +0200

    add POST checkout endpoint

--- src/stripe/stripe.controller.ts
+++ src/stripe/stripe.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { Cart } from './Cart.model';
+import { StripeService } from './stripe.service';
 
 @Controller('stripe')
-export class StripeController {}
+export class StripeController {
+  constructor(private stripeService: StripeService) {}
+
+  @Post()
+  checkout(@Body() body: { cart: Cart }) {
+    try {
+      return this.stripeService.checkout(body.cart);
+    } catch (error) {
+      return error;
+    }
+  }
+}
+
