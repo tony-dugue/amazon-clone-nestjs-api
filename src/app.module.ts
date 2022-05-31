2476c00f588440aea0ce7dace014031b7b386090
tony dugué <duguetony@gmail.com>
2022-06-01 00:24:35 +0200

    add stripe module (+ controller & service)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,20 @@
 import { Module } from '@nestjs/common';
-import {MongooseModule} from "@nestjs/mongoose";
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { StripeModule } from './stripe/stripe.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/amazon'),
     ProductModule,
     UserModule,
-    AuthModule
+    AuthModule,
+    StripeModule,
   ],
   controllers: [AppController],
   providers: [AppService],
